a9221c1139e7553bc0254125bc0c1882960c753f
fedepau <fedepau@art200.localdomain>
2022-01-12 15:37:41 +0100

    Implemented battery status

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"", "acpi -V | grep -m 1 \"Battery 0\" | awk '{print $3, substr($4, 1, length($4)-1)}'",	30,	0},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+
+	{"", "date '+%a %b %d %Y %H:%M'",					30,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
