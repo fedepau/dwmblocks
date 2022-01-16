d4a812237c9bead9a7a8ae608c37f05a54cd87f7
fedepau <fedepau@art200.localdomain>
2022-01-16 18:22:02 +0100

    Minor adjustments.

--- blocks.def.h
+++ blocks.def.h
@@ -3,9 +3,13 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "acpi -V | grep -m 1 \"Battery 0\" | awk '{print $3, substr($4, 1, length($4)-1)}'",	30,	0},
 
-	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	{"", "echo $(acpi -V | grep -m 1 \"Thermal 0\" | awk '{print $4}')°",				30,	0},
 
-	{"", "date '+%a %b %d %Y %H:%M'",					30,		0},
+	{"", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",					30,	0},
+
+	{"", "pamixer --get-volume-human",								30,	1},
+
+	{"", "date '+%d %b %Y (%a) %H:%M'",								30,	0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
