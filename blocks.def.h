e11b45b6b28b9acfcf2399641031039b8de09e46
fedepau <cpaulett@sissa.it>
2022-03-30 15:13:49 +0200

    Added some scripts.

--- blocks.def.h
+++ blocks.def.h
@@ -1,19 +1,21 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "acpi -V | grep -m 1 \"Battery 0\" | awk '{print $3, substr($4, 1, length($4)-1)}'",	30,	0},
+/*	{"", "acpi -V | grep -m 1 \"Battery 0\" | awk '{print $3, substr($4, 1, length($4)-1)}'",	30,	0}, */
 
-	{"", "echo $(acpi -V | grep -m 1 \"Thermal 0\" | awk '{print $4}')°",				30,	0},
+	{"", "battery.sh",								30,	0},
 
-	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",				30,	0},
+	{"", "thermal.sh",								30,	0},
 
-	{"", "volume.sh",										0,	10},
+	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",		30,	0},
 
-	{" ", "date '+%d %b %Y (%a)'",									0,	0},
+	{"", "volume.sh",								0,	10},
+
+	{" ", "date '+%d %b %Y (%a)'",							0,	0},
 	
-	{" ", "date '+%H:%M'",										60,	0},
+	{" ", "date '+%H:%M'",								60,	0},
 
-	{" ", "echo $(xbacklight -get | awk -F. '{print $1}')%",					0,	10},
+	{" ", "echo $(xbacklight -get | awk -F. '{print $1}')%",			0,	10},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
