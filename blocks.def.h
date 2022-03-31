6869e0306ad5f30c629063e3b46cb48ed0428fec
fedepau <cpaulett@sissa.it>
2022-03-31 10:21:45 +0200

    Added some scripts.

--- blocks.def.h
+++ blocks.def.h
@@ -3,19 +3,19 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 /*	{"", "acpi -V | grep -m 1 \"Battery 0\" | awk '{print $3, substr($4, 1, length($4)-1)}'",	30,	0}, */
 
-	{"", "battery.sh",								30,	0},
+	{"", "battery.sh",		30,	0},
 
-	{"", "thermal.sh",								30,	0},
+	{"", "thermal.sh",		30,	0},
 
-	{" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",		30,	0},
+	{"", "memory.sh",		30,	0},
 
-	{"", "volume.sh",								0,	10},
+	{"", "volume.sh",		0,	10},
 
-	{" ", "date '+%d %b %Y (%a)'",							0,	0},
+	{"", "calendar.sh",		0,	0},
 	
-	{" ", "date '+%H:%M'",								60,	0},
+	{"", "clock.sh",		60,	0},
 
-	{" ", "echo $(xbacklight -get | awk -F. '{print $1}')%",			0,	10},
+	{"", "brightness.sh",		0,	10},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
