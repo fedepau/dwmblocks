8143f2ee89e05b95da2e4cb3dd2347124a5781b8
Carlo Federico Pauletti <cpaulett@sissa.it>
2022-08-07 15:22:29 +0200

    added some modules

--- blocks.def.h
+++ blocks.def.h
@@ -3,19 +3,27 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 /*	{"", "acpi -V | grep -m 1 \"Battery 0\" | awk '{print $3, substr($4, 1, length($4)-1)}'",	30,	0}, */
 
-	{"", "battery.sh",		30,	0},
-
 	{"", "thermal.sh",		30,	0},
+
+	{"", "cpu.sh",			30,	0},
 
 	{"", "memory.sh",		30,	0},
 
+	{"", "swap.sh",			30,	0},
+
+	{"", "disk.sh",			600,	0},
+
 	{"", "volume.sh",		0,	10},
+
+	{"", "brightness.sh",		0,	10},
 
 	{"", "calendar.sh",		0,	0},
 	
 	{"", "clock.sh",		60,	0},
+	
+	{"", "battery.sh",		30,	0},
 
-	{"", "brightness.sh",		0,	10},
+	{"", "internet.sh",		30,     0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
