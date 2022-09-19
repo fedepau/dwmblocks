9a93d65e84c783cc9b01d42391bdd80d88c4d440
Carlo Federico Pauletti <cpaulett@sissa.it>
2022-09-19 14:13:58 +0200

    changes

--- blocks.def.h
+++ blocks.def.h
@@ -1,31 +1,28 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-/*	{"", "acpi -V | grep -m 1 \"Battery 0\" | awk '{print $3, substr($4, 1, length($4)-1)}'",	30,	0}, */
 
-	{"", "thermal.sh",		30,	0},
+	{"", "sb-thermal",		30,	0},
 
-	{"", "cpu.sh",			30,	0},
+	{"", "sb-cpu",			30,	0},
 
-	{"", "memory.sh",		30,	0},
+	{"", "sb-ram",  		30,	0},
 
-	{"", "swap.sh",			30,	0},
+	{"", "sb-swap",			30,	0},
 
-	{"", "disk.sh",			600,	0},
+	{"", "sb-disk",			600,	0},
 
-	{"", "volume.sh",		0,	10},
+	{"", "sb-volume",		0,	10},
 
-	{"", "brightness.sh",		0,	10},
+	{"", "sb-bright",	    0,	11},
 
-	{"", "calendar.sh",		0,	0},
+	{"", "sb-cal",		    0,	0},
 	
-	{"", "clock.sh",		60,	0},
+	{"", "sb-time",		    60,	0},
 	
-	{"", "battery.sh",		30,	0},
-
-	{"", "internet.sh",		30,     0},
+	{"", "sb-battery",	    30,	0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "\0";
 static unsigned int delimLen = 5;
