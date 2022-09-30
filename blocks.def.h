49a0a164c8bc7852a491ef792d43f13ff5ab1459
Carlo Federico Pauletti <cpaulett@sissa.it>
2022-09-30 16:45:00 +0200

    fixed statuscmd integration

--- blocks.def.h
+++ blocks.def.h
@@ -2,25 +2,25 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
-	{"", "sb-thermal",		30,	0},
+	{"", "sb-thermal",		30,	1},
 
-	{"", "sb-cpu",			30,	0},
+	{"", "sb-cpu",			30,	2},
 
-	{"", "sb-ram",  		30,	0},
+	{"", "sb-ram",  		30,	3},
 
-	{"", "sb-swap",			30,	0},
+	{"", "sb-swap",			30,	4},
 
-	{"", "sb-disk",			600,	0},
+	{"", "sb-disk",			600,	5},
 
 	{"", "sb-volume",		0,	10},
 
 	{"", "sb-bright",	    0,	11},
 
-	{"", "sb-cal",		    0,	0},
+	{"", "sb-cal",		    0,	6},
 	
-	{"", "sb-time",		    60,	0},
+	{"", "sb-time",		    60,	7},
 	
-    {"", "sb-battery",	    30,	0},
+    {"", "sb-battery",	    30,	8},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
